e7dff36b324454420b5c29eedd16177a528e0d9a
Kun Chen <kunchenguid@gmail.com>
2021-01-27 13:21:52 -0800

    minor refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import EventEmitter from "eventemitter3";
 import moment from "moment";
 import { CombatData } from "./CombatData";
-import { CombatUnitClass, CombatUnitSpec, ILogLine, LogEvent } from "./types";
+import { ILogLine, LogEvent } from "./types";
 
 export * from "./CombatData";
 export * from "./types";
@@ -28,15 +28,46 @@ export class WoWCombatLogParser extends EventEmitter {
     this.lastTimestamp = 0;
     this.state = LogParsingState.NOT_IN_MATCH;
 
-    this.endCurrentCombat(["-1", "0", "0", "0"]);
+    this.endCurrentCombat();
   }
 
   public parseLine(line: string): void {
-    const regex_matches = line.match(WoWCombatLogParser.LINE_PARSER);
+    const logLine = this.parseLogLine(line);
 
     // skip if it's not a valid line
-    if (!regex_matches || regex_matches.length === 0) {
+    if (!logLine) {
       return;
+    }
+
+    const timeout =
+      logLine.timestamp - this.lastTimestamp >
+      WoWCombatLogParser.COMBAT_AUTO_TIMEOUT_SECS * 1000;
+
+    if (timeout) {
+      this.endCurrentCombat();
+    }
+
+    if (this.state === LogParsingState.NOT_IN_MATCH) {
+      if (logLine.event === LogEvent.ARENA_MATCH_START) {
+        this.startNewCombat(logLine);
+      }
+    } else {
+      if (logLine.event === LogEvent.ARENA_MATCH_END) {
+        this.endCurrentCombat(logLine);
+      } else {
+        this.processLogLine(logLine);
+      }
+    }
+
+    this.lastTimestamp = logLine.timestamp;
+  }
+
+  private parseLogLine(line: string): ILogLine | null {
+    const regex_matches = line.match(WoWCombatLogParser.LINE_PARSER);
+
+    // not a valid line
+    if (!regex_matches || regex_matches.length === 0) {
+      return null;
     }
 
     const month = parseInt(regex_matches[1], 10);
@@ -46,7 +77,14 @@ export class WoWCombatLogParser extends EventEmitter {
     const second = parseInt(regex_matches[5], 10);
     const ms = parseInt(regex_matches[6], 10);
 
-    const event = regex_matches[7];
+    const eventName = regex_matches[7];
+
+    // unsupported event
+    if (!(eventName in LogEvent)) {
+      return null;
+    }
+    const event = LogEvent[eventName as keyof typeof LogEvent];
+
     const timestampValueObj = {
       ms,
       M: month - 1,
@@ -58,144 +96,39 @@ export class WoWCombatLogParser extends EventEmitter {
     const timestampValue = moment(timestampValueObj);
     const timestamp = timestampValue.valueOf();
 
-    const timeout =
-      timestamp - this.lastTimestamp >
-      WoWCombatLogParser.COMBAT_AUTO_TIMEOUT_SECS * 1000;
+    const parameters = regex_matches[8].split(",");
 
-    const params = regex_matches[8].split(",");
-
-    if (timeout || this.state === LogParsingState.NOT_IN_MATCH) {
-      if (timeout) {
-        this.endCurrentCombat(["-1", "0", "0", "0"]);
-      }
-
-      if (event === "ARENA_MATCH_START") {
-        this.startNewCombat(timestamp, params);
-      }
-    } else {
-      if (event === "ARENA_MATCH_END") {
-        this.endCurrentCombat(params);
-      } else {
-        this.processCombatLogLine(timestamp, event, params);
-      }
-    }
-
-    this.lastTimestamp = timestamp;
-  }
-
-  private processCombatLogLine(
-    timestamp: number,
-    eventName: string,
-    parameters: string[]
-  ): void {
-    if (!this.currentCombat) {
-      return;
-    }
-
-    if (eventName === "COMBATANT_INFO") {
-      const unitId = parameters[0];
-      const specId = parseInt(parameters[23], 10);
-      if (specId in CombatUnitSpec) {
-        const spec = specId as CombatUnitSpec;
-        let unitClass = CombatUnitClass.None;
-        switch (spec) {
-          case CombatUnitSpec.DeathKnight_Blood:
-          case CombatUnitSpec.DeathKnight_Frost:
-          case CombatUnitSpec.DeathKnight_Unholy:
-            unitClass = CombatUnitClass.DeathKnight;
-            break;
-          case CombatUnitSpec.DemonHunter_Havoc:
-          case CombatUnitSpec.DemonHunter_Vengeance:
-            unitClass = CombatUnitClass.DemonHunter;
-            break;
-          case CombatUnitSpec.Druid_Balance:
-          case CombatUnitSpec.Druid_Feral:
-          case CombatUnitSpec.Druid_Guardian:
-          case CombatUnitSpec.Druid_Restoration:
-            unitClass = CombatUnitClass.Druid;
-            break;
-          case CombatUnitSpec.Hunter_BeastMastery:
-          case CombatUnitSpec.Hunter_Marksmanship:
-          case CombatUnitSpec.Hunter_Survival:
-            unitClass = CombatUnitClass.Hunter;
-            break;
-          case CombatUnitSpec.Mage_Arcane:
-          case CombatUnitSpec.Mage_Fire:
-          case CombatUnitSpec.Mage_Frost:
-            unitClass = CombatUnitClass.Mage;
-            break;
-          case CombatUnitSpec.Monk_BrewMaster:
-          case CombatUnitSpec.Monk_Windwalker:
-          case CombatUnitSpec.Monk_Mistweaver:
-            unitClass = CombatUnitClass.Monk;
-            break;
-          case CombatUnitSpec.Paladin_Holy:
-          case CombatUnitSpec.Paladin_Protection:
-          case CombatUnitSpec.Paladin_Retribution:
-            unitClass = CombatUnitClass.Paladin;
-            break;
-          case CombatUnitSpec.Priest_Discipline:
-          case CombatUnitSpec.Priest_Holy:
-          case CombatUnitSpec.Priest_Shadow:
-            unitClass = CombatUnitClass.Priest;
-            break;
-          case CombatUnitSpec.Rogue_Assassination:
-          case CombatUnitSpec.Rogue_Outlaw:
-          case CombatUnitSpec.Rogue_Subtlety:
-            unitClass = CombatUnitClass.Rogue;
-            break;
-          case CombatUnitSpec.Shaman_Elemental:
-          case CombatUnitSpec.Shaman_Enhancement:
-          case CombatUnitSpec.Shaman_Restoration:
-            unitClass = CombatUnitClass.Shaman;
-            break;
-          case CombatUnitSpec.Warlock_Affliction:
-          case CombatUnitSpec.Warlock_Demonology:
-          case CombatUnitSpec.Warlock_Destruction:
-            unitClass = CombatUnitClass.Warlock;
-            break;
-          case CombatUnitSpec.Warrior_Arms:
-          case CombatUnitSpec.Warrior_Fury:
-          case CombatUnitSpec.Warrior_Protection:
-            unitClass = CombatUnitClass.Warrior;
-            break;
-        }
-        this.currentCombat.registerCombatant(unitId, {
-          spec,
-          class: unitClass,
-        });
-      }
-      return;
-    }
-
-    if (!(eventName in LogEvent)) {
-      return;
-    }
-
-    const event = LogEvent[eventName as keyof typeof LogEvent];
-
-    const logLine: ILogLine = {
+    return {
       id: (WoWCombatLogParser.nextId++).toFixed(),
       timestamp,
       event,
       parameters,
     };
+  }
+
+  private processLogLine(logLine: ILogLine): void {
+    if (!this.currentCombat) {
+      return;
+    }
 
     this.currentCombat.readLogLine(logLine);
     this.emit("arena_match_updated", this.currentCombat);
   }
 
-  private startNewCombat(timestamp: number, params: string[]): void {
+  private startNewCombat(logLine: ILogLine): void {
     this.currentCombat = new CombatData();
-    this.currentCombat.startTime = timestamp || 0;
-    this.currentCombat.playerTeamId = parseInt(params[3]);
+    this.currentCombat.startTime = logLine.timestamp || 0;
+    this.currentCombat.playerTeamId = parseInt(logLine.parameters[3]);
     this.state = LogParsingState.IN_MATCH;
     this.emit("arena_match_started", this.currentCombat);
   }
 
-  private endCurrentCombat(params: string[]): void {
+  private endCurrentCombat(logLine?: ILogLine): void {
     if (this.currentCombat) {
-      this.currentCombat.end([parseInt(params[2]), parseInt(params[3])]);
+      this.currentCombat.end([
+        parseInt(logLine ? logLine.parameters[2] : "0"), // team0 rating
+        parseInt(logLine ? logLine.parameters[3] : "0"), // team1 rating
+      ]);
       this.emit("arena_match_ended", this.currentCombat);
       this.currentCombat = null;
     }
